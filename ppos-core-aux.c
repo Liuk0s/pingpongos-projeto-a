7967546651535c28c8a64ed83da71e32a7b68dc4
Liuk0s <lucas.thuler@protonmail.com>
2025-06-29 20:05:44 -0300

    Update ppos-core-aux.c

--- ppos-core-aux.c
+++ ppos-core-aux.c
@@ -307,13 +307,13 @@ void after_task_exit () {
         task_total_time,        
         taskExec->proc_time,    
         taskExec->activations);
-}
 
 //Se a main (task 0) terminou, sinaliza disk manager para encerrar
     if (taskExec->id == 0) {
         extern void disk_mgr_shutdown();  // Declara função do ppos_disk.c
         disk_mgr_shutdown();
     }
+}
 
 /**
  * Contabiliza tempo da tarefa que está saindo do processador
